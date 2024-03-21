56613f8cdb2120a02671ab64ec5cb2c7e0f7ca3e
petrfehnl <101090891+petrfehnl@users.noreply.github.com>
2024-03-21 08:02:07 +0100

    Update MainWindow.xaml.cs

--- ukolos/MainWindow.xaml.cs
+++ ukolos/MainWindow.xaml.cs
@@ -1,28 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ukolos
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                border.Background = Brushes.LightGreen; // Změna pozadí při MouseEnter
+            }
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                border.Background = Brushes.LightBlue; // Změna pozadí při MouseLeave
+            }
+        }
+
+        private void Border_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                double step = 10;
+                switch (e.Key)
+                {
+                    case Key.W:
+                        Canvas.SetTop(border, Canvas.GetTop(border) - step);
+                        break;
+                    case Key.S:
+                        Canvas.SetTop(border, Canvas.GetTop(border) + step);
+                        break;
+                    case Key.A:
+                        Canvas.SetLeft(border, Canvas.GetLeft(border) - step);
+                        break;
+                    case Key.D:
+                        Canvas.SetLeft(border, Canvas.GetLeft(border) + step);
+                        break;
+                }
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                // Aktualizace textu bez použití Click události
+                // Zde můžete provést jakoukoli další logiku aktualizace textu
+            }
         }
     }
 }
